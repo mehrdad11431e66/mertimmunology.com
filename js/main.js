34e6d1fd3fc911de9f80c0c40991affe0ea2170f
mehrdad11431e66 <mehrdad.etemad1988@gmail.com>
2025-04-26 12:43:21 +0300

    Update main.js
    Signed-off-by: mehrdad11431e66 <mehrdad.etemad1988@gmail.com>

--- js/main.js
+++ js/main.js
@@ -16,19 +16,76 @@
         }
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
+
+        // Initialize modules
+        initLanguageSwitcher();
+        initGallery();
+        initVideos();
+        initAnimations();
     });
 
+    // Language Switcher (English/Turkish/Arabic)
+    function initLanguageSwitcher() {
+        $('.language-switcher').on('click', 'a', function(e) {
+            e.preventDefault();
+            const lang = $(this).data('lang');
+            switchLanguage(lang);
+        });
 
-    // Date and time picker
-    $('.date').datetimepicker({
-        format: 'L'
-    });
-    $('.time').datetimepicker({
-        format: 'LT'
-    });
-    
-    
-    // Back to top button
+        function switchLanguage(lang) {
+            // RTL/LTR handling
+            if (lang === 'ar') {
+                $('html').attr('dir', 'rtl').attr('lang', 'ar');
+            } else {
+                $('html').attr('dir', 'ltr').attr('lang', lang);
+            }
+            
+            // Here you would typically load language JSON files
+            console.log(`Switched to ${lang} - Load your translations here`);
+        }
+    }
+
+    // Gallery System (No image modifications)
+    function initGallery() {
+        $('.gallery-item').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+            callbacks: {
+                beforeOpen: function() {
+                    // Your custom gallery logic
+                }
+            }
+        });
+    }
+
+    // Video Player System
+    function initVideos() {
+        $('.video-player').each(function() {
+            $(this).on('click', function() {
+                const videoSrc = $(this).data('src');
+                $(this).html(`
+                    <iframe src="${videoSrc}?autoplay=1" frameborder="0" allowfullscreen></iframe>
+                `);
+            });
+        });
+    }
+
+    // Animation System
+    function initAnimations() {
+        $('[data-animate]').waypoint(function() {
+            $(this.element).addClass('animated ' + $(this.element).data('animate'));
+        }, {
+            offset: '90%',
+            triggerOnce: true
+        });
+    }
+
+    // Original Carousels and Pickers (Keep all your existing code below)
+    $('.date').datetimepicker({ format: 'L' });
+    $('.time').datetimepicker({ format: 'LT' });
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
@@ -36,13 +93,12 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+    
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
-
-    // Price carousel
     $(".price-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -55,20 +111,12 @@
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:3
-            }
+            0:{ items:1 },
+            992:{ items:2 },
+            1200:{ items:3 }
         }
     });
 
-
-    // Team carousel
     $(".team-carousel, .related-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -81,24 +129,17 @@
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            }
+            0:{ items:1 },
+            992:{ items:2 }
         }
     });
 
-
-    // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         items: 1,
         dots: true,
-        loop: true,
+        loop: true
     });
-    
-})(jQuery);
 
+})(jQuery);
